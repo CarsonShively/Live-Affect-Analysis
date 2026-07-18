3d54923ae53aee4d3fd3984ec209e8b2f7351733
Carson Shively <carsonshively24@gmail.com>
2026-07-17 21:10:42 -0700

    backup

--- process_images.py
+++ process_images.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 import shutil
+from live_affect_analysis.map import category_map, satisfaction_map, calmness_map
 
 
 def process_images():
@@ -20,35 +21,66 @@ def process_images():
         )
     
     df = pd.read_csv(labels_path)
+    df = df.sample(frac=1, random_state=42).reset_index(drop=True)
     
     print("data mounted")
     
-    images_count = 0
+    samples_count = len(df)
     
-    for image in images_path.iterdir():
-        if image.is_file():
-            images_count += 1
-            
-    print(f"images count: {images_count}")
+    train_size = int(samples_count * 0.70)
+    val_size = int(samples_count * 0.10)
+    test_size = samples_count - train_size - val_size 
     
-    out_path = Path("/content/drive/MyDrive/Live-Affect-Analysis/processed_images")
-    if out_path.is_dir():
-        shutil.rmtree(out_path)
-    out_path.mkdir(parents=True, exist_ok=True)
+    local_out = Path("/content/matrices")
+    if local_out.is_dir():
+        shutil.rmtree(local_out)
+    local_out.mkdir(parents=True, exist_ok=True)
+    
+    train_images = np.lib.format.open_memmap(
+        local_out / "train.npy",
+        mode="w+",
+        dtype=np.uint8,
+        shape=(train_size, 224, 224, 3)
+    )
+    
+    val_images = np.lib.format.open_memmap(
+        local_out / "val.npy",
+        mode="w+",
+        dtype=np.uint8,
+        shape=(val_size, 224, 224, 3)
+    )
+    test_images = np.lib.format.open_memmap(
+        local_out / "test.npy",
+        mode="w+",
+        dtype=np.uint8,
+        shape=(test_size, 224, 224, 3)
+    )
+    
+    train_labels = []
+    val_labels = []
+    test_labels = []
+       
+    print(f"samples count: {samples_count}")
 
-    for image_path in local_images_path.iterdir():
-        name = image_path.stem
+    train_counter = 0
+    val_counter = 0
+    test_counter = 0
+    for _, row in df.iterrows():
         try:
+            image_path = local_images_path / row["Image"]
+            
             if not image_path.is_file():
                 continue
             image = cv2.imread(str(image_path))
             if image is None:
                 continue
-            label_row = df[df["Image"] == image_path.name]
-            x_min = int(label_row["xmin"].iloc[0])
-            x_max = int(label_row["xmax"].iloc[0])
-            y_min = int(label_row["ymin"].iloc[0])
-            y_max = int(label_row["ymax"].iloc[0])
+            
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            
+            x_min = int(row["xmin"])
+            x_max = int(row["xmax"])
+            y_min = int(row["ymin"])
+            y_max = int(row["ymax"])
             cropped_image = image[y_min:y_max, x_min:x_max]
             image_tensor = tf.convert_to_tensor(cropped_image, dtype=tf.float32)
             image_tensor_resized = tf.image.resize_with_pad(
@@ -57,19 +89,100 @@ def process_images():
                 target_width=224
             )
             
-            image_tensor_normalized = image_tensor_resized / 255.0
+            image_numpy = tf.cast(
+                tf.round(image_tensor_resized),
+                tf.uint8
+            ).numpy()
             
-            image_numpy = image_tensor_normalized.numpy()
-            
-            np.save(out_path / f"{name}.npy", image_numpy)
+            labels = []
 
-            images_count -= 1
-            print(f"{images_count} images remaining")
+            labels.append(category_map[row["Category"]])
+            labels.append(satisfaction_map[row["Label_SA"]])
+            labels.append(calmness_map[row["Label_CA"]])
+            labels.append(row["Valence"])
+            labels.append(row["Arousal"])
+            labels.append(row["Dominance"])
+
+            if train_counter < train_size:
+                train_images[train_counter] = image_numpy
+                train_counter += 1
+                print(f"train remaining: {train_size - train_counter}")
+                train_labels.append(labels)
+            elif val_counter < val_size:
+                val_images[val_counter] = image_numpy
+                val_counter += 1
+                print(f"val remaining: {val_size - val_counter}")
+                val_labels.append(labels)
+            else:
+                test_images[test_counter] = image_numpy
+                test_counter += 1
+                test_remaining = test_size - test_counter
+                print(f"test remaining: {test_remaining}")
+                test_labels.append(labels)
+
         except Exception as error:
             print(f"Error file: {image_path.name}: {error}")
             continue
         
-
+    test_images.flush()
+        
+    trimmed_test = np.lib.format.open_memmap(
+        local_out / "test_trimmed.npy",
+        mode="w+",
+        dtype=np.uint8,
+        shape=(test_counter, 224, 224, 3)
+    )
+    
+    trimmed_test[:] = test_images[:test_counter]
+    
+    train_images.flush()
+    val_images.flush()
+    trimmed_test.flush()
+        
+    train_labels_matrix = np.array(train_labels)
+    val_labels_matrix = np.array(val_labels)
+    test_labels_matrix = np.array(test_labels)
+    
+    drive_modeling_out = Path("/content/drive/MyDrive/Live-Affect-Analysis/modeling_matrices")
+    drive_eval_out = Path("/content/drive/MyDrive/Live-Affect-Analysis/eval_matrices")
+    
+    if drive_modeling_out.is_dir():
+        shutil.rmtree(drive_modeling_out)
+        
+    if drive_eval_out.is_dir():
+        shutil.rmtree(drive_eval_out)
+        
+    drive_modeling_out.mkdir(parents=True, exist_ok=True)
+    drive_eval_out.mkdir(parents=True, exist_ok=True)
+    
+    shutil.copy2(
+        local_out / "train.npy",
+        drive_modeling_out / "train_images.npy"
+    )
+    
+    print("train images complete")
+    
+    shutil.copy2(
+        local_out / "val.npy",
+        drive_modeling_out / "val_images.npy"
+    )
+    
+    print("val images complete")
+    
+    np.save(drive_modeling_out / "train_labels.npy", train_labels_matrix)
+    np.save(drive_modeling_out / "val_labels.npy", val_labels_matrix)
+    print("train and val labels complete")
+    
+    shutil.copy2(
+        local_out / "test_trimmed.npy",
+        drive_eval_out / "test_images.npy"
+    )
+    
+    print("test images complete")
+    
+    np.save(drive_eval_out / "test_labels.npy", test_labels_matrix)
+    print("test labels complete")
+    print("complete")
         
 if __name__ == "__main__":
     process_images()
