84a73b17446b89c933971e4d2d8bacad11124c0a
Carson Shively <carsonshively24@gmail.com>
2026-07-19 03:40:43 -0700

    backup

--- matrices_eda.py
+++ matrices_eda.py
@@ -2,29 +2,29 @@ import numpy as np
 from pathlib import Path
 
 def matrices_eda():
-    train_path = Path("/content/drive/MyDrive/Affect-Analysis/modeling_matrices")
-    eval_path = Path("/content/drive/MyDrive/Affect-Analysis/eval_matrices")
+    modeling_path = Path("/content/drive/MyDrive/Live-Affect-Analysis/modeling_matrices")
+    eval_path = Path("/content/drive/MyDrive/Live-Affect-Analysis/eval_matrices")
     
-    train_matrix = np.load(train_path / "train.npy")
-    print(f"train images shape: {train_matrix.shape}")
-    del train_matrix
+    train_images = np.load(modeling_path / "train_images.npy")
+    print(f"train images shape: {train_images.shape}")
+    del train_images
     
-    val_matrix = np.load(train_path / "val.npy")
-    print(f"val images shape: {val_matrix.shape}")
-    del val_matrix
+    val_images = np.load(modeling_path / "val_images.npy")
+    print(f"val images shape: {val_images.shape}")
+    del val_images
     
-    test_matrix = np.load(eval_path / "test.npy")
-    print(f"test shape: {test_matrix.shape}")
-    del test_matrix
+    test_images = np.load(eval_path / "test_images.npy")
+    print(f"test images shape: {test_images.shape}")
+    del test_images
     
-    train_labels = np.load(train_path / "train_labels.npy")
-    print(f"train labels: {train_labels.shape}")
+    train_labels = np.load(modeling_path / "train_labels.npy")
+    print(f"train labels shpae: {train_labels.shape}")
     
-    train_labels = np.load(train_path / "train_labels.npy")
-    print(f"train labels: {train_labels.shape}")
+    val_labels = np.load(modeling_path / "val_labels.npy")
+    print(f"val labels shpae: {val_labels.shape}")
     
-    train_labels = np.load(train_path / "train_labels.npy")
-    print(f"train labels: {train_labels.shape}")
-    
+    test_labels = np.load(eval_path / "test_labels.npy")
+    print(f"train labels shpae: {test_labels.shape}")
+
 if __name__ == "__main__":
     matrices_eda()
