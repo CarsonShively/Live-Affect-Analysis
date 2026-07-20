7fb7eda367cd5fb4b2e38f4ad04a00589fa99ddb
Carson Shively <carsonshively24@gmail.com>
2026-07-20 07:08:33 -0700

    backup

--- inspect_annotations.py
+++ inspect_annotations.py
@@ -10,32 +10,7 @@ def inspect_annotations():
      
     train = annotations["train"]
     
-    print(f"train type: {type(train)}")
-    print(f"train shape: {train.shape}")
-    print(f"train item type {type(train[0])}")
-    print(f"train item fields {train[0]._fieldnames}")
-    
-    print(f"filename {type(train[0].filename)}")
-    print(f"folder {type(train[0].folder)}")
-    print(f"image size {type(train[0].image_size)}")
-    print(f"original database {type(train[0].original_database)}")
-    print(f"person {type(train[0].person)}")
-     
-    print(f"image size fields {(train[0].image_size)._fieldnames}")
-    print(f"original database fields {(train[0].original_database)._fieldnames}")
-    print(f"person fields {(train[0].person)._fieldnames}")
-    
-    print(f"image size ncol nrow types {type((train[0].image_size).n_col)}, {type((train[0].image_size).n_row)}")
-    print(f"original databse field types {type((train[0].original_database).name)}, {type((train[0].original_database).info)}")
-    
-    print(f"person field types {type((train[0].person).body_bbox)}, {type((train[0].person).annotations_categories)}, {type((train[0].person).annotations_continuous)}, {type((train[0].person).gender)}, {type((train[0].person).age)}")
-    
-    print(f"bbox item {type(((train[0].person).body_bbox)[0])}")
-    print(f"bbox len {len((train[0].person).body_bbox)}")
-    
-    print(f"cat fields {((train[0].person).annotations_categories)._fieldnames}")
-    print(f"cat type {type(((train[0].person).annotations_categories).categories)}")
-    print(f"cat item type {((((train[0].person).annotations_categories).categories)[0]).fieldnames}")
+    print(train[0].person.annotations_categories._fieldnames)
     
 if __name__ == "__main__":
     inspect_annotations()
