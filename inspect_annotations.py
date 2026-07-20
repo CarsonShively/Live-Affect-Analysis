8789d11b19281f5c92d1393e4ad1c0bed68b1d7d
Carson Shively <carsonshively24@gmail.com>
2026-07-20 04:18:11 -0700

    backup

--- inspect_annotations.py
+++ inspect_annotations.py
@@ -1,10 +1,17 @@
-from scipy.io import whosmat
+from scipy.io import loadmat, whosmat
 from pathlib import Path
 
 def inspect_annotations():
     mat_path = Path("/content/drive/MyDrive/Live-Affect-Analysis/emotica/annotations/Annotations.mat")
     
     print(whosmat(mat_path))
+    
+    annotations = loadmat(mat_path, structs_as_record=False, squeeze_me=True)
+     
+    train = annotations["train"]
+    
+    print(f"train type: {type(train)}")
+    print(f"train shape: {train.shape}")
      
 if __name__ == "__main__":
     inspect_annotations()
