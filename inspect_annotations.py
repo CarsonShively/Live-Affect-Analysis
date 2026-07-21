97d6bb9c52d094241cbc017052776ff031f93164
Carson Shively <carsonshively24@gmail.com>
2026-07-21 01:48:12 -0700

    backup

--- inspect_annotations.py
+++ inspect_annotations.py
@@ -10,7 +10,7 @@ def inspect_annotations():
      
     test = annotations["test"]
     
-    print(test[0].person.annotations_continuous._fieldnames)
+    print(test[0].person.annotations_continuous[0]._fieldnames)
     
 if __name__ == "__main__":
     inspect_annotations()
