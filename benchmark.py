36b7bd509c3124d3f1e5a776d7ce92f48d6f3548
Carson Shively <carsonshively24@gmail.com>
2026-07-24 00:25:06 -0700

    backup

--- benchmark.py
+++ benchmark.py
@@ -16,53 +16,42 @@ def benchmark():
     val_labels_valence = (val_labels[:, 26:27] - 1.0) / 9.0
     val_labels_arousal = (val_labels[:, 27:28] - 1.0) / 9.0
     
-    train_confusion = train_labels[:, 1]
-    train_happiness = np.max(train_labels[:, [5, 7, 4]], axis=1, keepdims=True)
-    train_connfidence = np.max(train_labels[:, [8, 19, 3]], axis=1, keepdims=True)
-    train_calmness = train_labels[:, 9]
-    train_distress = np.max(train_labels[:, [11, 14, 12, 18, 24]], axis=1, keepdims=True)
-    train_fear = np.max(train_labels[:, [16, 25]], axis=1, keepdims=True)
-    train_anger = np.max(train_labels[:, [22, 20, 6, 23]], axis=1, keepdims=True)
+    train_happiness = train_labels[:, 5:6]
+    train_calmness = train_labels[:, 9:10]
+    train_sadness = train_labels[:, 12:13]
+    train_fear = train_labels[:, 16:17]
+    train_anger = train_labels[:, 22:23]
     
-    val_confusion = val_labels[:, 1]
-    val_happiness = np.max(val_labels[:, [5, 7, 4]], axis=1, keepdims=True)
-    val_connfidence = np.max(val_labels[:, [8, 19, 3]], axis=1, keepdims=True)
-    val_calmness = val_labels[:, 9]
-    val_distress = np.max(val_labels[:, [11, 14, 12, 18, 24]], axis=1, keepdims=True)
-    val_fear = np.max(val_labels[:, [16, 25]], axis=1, keepdims=True)
-    val_anger = np.max(val_labels[:, [22, 20, 6, 23]], axis=1, keepdims=True)
+    val_happiness = val_labels[:, 5:6]
+    val_calmness = val_labels[:, 9:10]
+    val_sadness = val_labels[:, 12:13]
+    val_fear = val_labels[:, 16:17]
+    val_anger = val_labels[:, 22:23]
     
 
-    train_confusion = tf.convert_to_tensor(train_confusion, dtype=tf.float32)
     train_happiness = tf.convert_to_tensor(train_happiness, dtype=tf.float32)
-    train_connfidence = tf.convert_to_tensor(train_connfidence, dtype=tf.float32)
     train_calmness = tf.convert_to_tensor(train_calmness, dtype=tf.float32)
-    train_distress = tf.convert_to_tensor(train_distress, dtype=tf.float32)
+    train_sadness = tf.convert_to_tensor(train_sadness, dtype=tf.float32)
     train_fear = tf.convert_to_tensor(train_fear, dtype=tf.float32)
     train_anger = tf.convert_to_tensor(train_anger, dtype=tf.float32)
     train_labels_valence = tf.convert_to_tensor(train_labels_valence, dtype=tf.float32)
     train_labels_arousal = tf.convert_to_tensor(train_labels_arousal, dtype=tf.float32)
     
     
-    val_confusion = tf.convert_to_tensor(val_confusion, dtype=tf.float32)
     val_happiness = tf.convert_to_tensor(val_happiness, dtype=tf.float32)
-    val_connfidence = tf.convert_to_tensor(val_connfidence, dtype=tf.float32)
     val_calmness = tf.convert_to_tensor(val_calmness, dtype=tf.float32)
-    val_distress = tf.convert_to_tensor(val_distress, dtype=tf.float32)
+    val_sadness = tf.convert_to_tensor(val_sadness, dtype=tf.float32)
     val_fear = tf.convert_to_tensor(val_fear, dtype=tf.float32)
     val_anger = tf.convert_to_tensor(val_anger, dtype=tf.float32)
     val_labels_valence = tf.convert_to_tensor(val_labels_valence, dtype=tf.float32)
     val_labels_arousal = tf.convert_to_tensor(val_labels_arousal, dtype=tf.float32)
 
 
-    mean_confusion = tf.fill(tf.shape(val_confusion), tf.mean(train_confusion))
     mean_hapiness = tf.fill(tf.shape(val_happiness), tf.mean(train_happiness))
-    mean_confidence = tf.fill(tf.shape(val_connfidence), tf.mean(train_connfidence))
     mean_calmness = tf.fill(tf.shape(val_calmness), tf.mean(train_calmness))
-    mean_distress = tf.fill(tf.shape(val_distress), tf.reduce_mean(train_distress))
+    mean_sadness = tf.fill(tf.shape(val_sadness), tf.reduce_mean(train_sadness))
     mean_fear = tf.fill(tf.shape(val_fear), tf.mean(train_fear))
     mean_anger = tf.fill(tf.shape(val_anger), tf.mean(train_anger))
-
     mean_val = tf.fill(tf.shape(val_labels_valence), tf.reduce_mean(train_labels_valence))
     mean_ar = tf.fill(tf.shape(val_labels_arousal), tf.reduce_mean(train_labels_arousal))
 
@@ -76,29 +65,25 @@ def benchmark():
     
     
     
-    conu_loss = bce(val_confusion, mean_confusion)
-    h_loss = bce(val_happiness, mean_hapiness)
-    ci_loss = bce(val_connfidence, mean_confidence)
-    ca_loss = bce(val_calmness, mean_calmness)
-    di_loss = bce(val_distress, mean_distress)
-    f_loss = bce(val_fear, mean_fear)
-    a_loss = bce(val_anger, mean_anger)
-    val_loss = mse(val_labels_valence, mean_val)
-    ar_loss = mse(val_labels_arousal, mean_ar)
+    happiness_loss = bce(val_happiness, mean_hapiness)
+    calmness_loss = bce(val_calmness, mean_calmness)
+    sadness_loss = bce(val_sadness, mean_sadness)
+    fear_loss = bce(val_fear, mean_fear)
+    anger_loss = bce(val_anger, mean_anger)
+    valence_loss = mse(val_labels_valence, mean_val)
+    arousal_loss = mse(val_labels_arousal, mean_ar)
     
-    loss = conu_loss + h_loss + ci_loss + ca_loss + di_loss + f_loss + a_loss + val_loss * 10.0 + ar_loss * 10.0
+    loss = happiness_loss + calmness_loss + sadness_loss + fear_loss + anger_loss + valence_loss * 10.0 + arousal_loss * 10.0
     
     report = {
         "total_loss": loss.numpy().item(),
-        "confusion_bce": conu_loss.numpy().item(),
-        "happiness_bce": h_loss.numpy().item(),
-        "confidence_bce": ci_loss.numpy().item(),
-        "calmness_bce": ca_loss.numpy().item(),
-        "distress_bce": di_loss.numpy().item(),
-        "fear_bce": f_loss.numpy().item(),
-        "anger_bce": a_loss.numpy().item(),
-        "valence_mse": val_loss.numpy().item(),
-        "arousal_mse": ar_loss.numpy().item()
+        "happiness_bce": happiness_loss.numpy().item(),
+        "calmness_bce": calmness_loss.numpy().item(),
+        "sadness_bce": sadness_loss.numpy().item(),
+        "fear_bce": fear_loss.numpy().item(),
+        "anger_bce": anger_loss.numpy().item(),
+        "valence_mse": valence_loss.numpy().item(),
+        "arousal_mse": arousal_loss.numpy().item()
     }
     
     out_path = Path("/content/benchmark.json")
