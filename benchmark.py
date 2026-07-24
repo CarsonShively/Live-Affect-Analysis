4ccd18dc979d8aeb0966f9e8a983b21e24c1d3e5
Carson Shively <carsonshively24@gmail.com>
2026-07-23 20:21:07 -0700

    backup

--- benchmark.py
+++ benchmark.py
@@ -15,40 +15,22 @@ def benchmark():
     train_category = tf.convert_to_tensor(train_labels[:, :26], dtype=tf.float32)
     train_valence = tf.convert_to_tensor(train_labels[:, 26:27], dtype=tf.float32)
     train_arousal = tf.convert_to_tensor(train_labels[:, 27:28], dtype=tf.float32)
-    train_dominance = tf.convert_to_tensor(train_labels[:, 28:29], dtype=tf.float32)
-    train_gender = tf.convert_to_tensor(train_labels[:, 29:30], dtype=tf.float32)
-    train_age = tf.convert_to_tensor(train_labels[:, 30], dtype=tf.int32)
 
     val_category = tf.convert_to_tensor(val_labels[:, :26], dtype=tf.float32)
     val_valence = tf.convert_to_tensor(val_labels[:, 26:27], dtype=tf.float32)
     val_arousal = tf.convert_to_tensor(val_labels[:, 27:28], dtype=tf.float32)
-    val_dominance = tf.convert_to_tensor(val_labels[:, 28:29], dtype=tf.float32)
-    val_gender = tf.convert_to_tensor(val_labels[:, 29:30], dtype=tf.float32)
-    val_age = tf.convert_to_tensor(val_labels[:, 30], dtype=tf.int32)
 
     category_probabilities = tf.reduce_mean(train_category, axis=0)
     mean_categories = tf.broadcast_to(category_probabilities, val_category.shape)
 
-    gender_prob = tf.reduce_mean(train_gender, axis=0)
-    gender_mean = tf.broadcast_to(gender_prob, val_gender.shape)
+
 
     mean_val = tf.fill(tf.shape(val_valence), tf.reduce_mean(train_valence))
     mean_ar = tf.fill(tf.shape(val_arousal), tf.reduce_mean(train_arousal))
-    mean_dom = tf.fill(tf.shape(val_dominance), tf.reduce_mean(train_dominance))
+
     
 
-    age_classes = tf.math.bincount(
-        train_age,
-        minlength=3,
-        maxlength=3,
-        dtype=tf.float32
-    )
-    
-    age_prob = age_classes / tf.reduce_sum(age_classes)
-    mean_age = tf.broadcast_to(
-        age_prob,
-        (tf.shape(val_age)[0], 3)
-    )
+
     
     
     sce = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
@@ -60,20 +42,14 @@ def benchmark():
     cat_loss = bce(val_category, mean_categories)
     val_loss = mse(val_valence, mean_val)
     ar_loss = mse(val_arousal, mean_ar)
-    dom_loss = mse(val_dominance, mean_dom)
-    gender_loss = bce(val_gender, gender_mean)
-    age_loss = sce(val_age, mean_age)
     
-    loss = cat_loss + gender_loss + age_loss + val_loss + ar_loss + dom_loss
+    loss = cat_loss + val_loss + ar_loss
     
     report = {
         "total_loss": loss.numpy().item(),
         "category_bce": cat_loss.numpy().item(),
         "valence_mse": val_loss.numpy().item(),
-        "arousal_mse": ar_loss.numpy().item(),
-        "dominance": dom_loss.numpy().item(),
-        "gender_bce": gender_loss.numpy().item(),
-        "age_sce": age_loss.numpy().item()
+        "arousal_mse": ar_loss.numpy().item()
     }
     
     out_path = Path("/content/benchmark.json")
